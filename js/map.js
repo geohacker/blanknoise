ba81dcf783e056ed70336dd89cb97eff00942537
Sajjad Anwar <sajjadkm@gmail.com>
2014-03-07 19:38:27 +0530

    First cut and b/w map

---
+++ js/map.js
@@ -0,0 +1,16 @@
+var map = L.map('map');
+var india = L.latLng([21.882, 82.749]);
+
+var mapquestUrl = 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
+subDomains = ['otile1','otile2','otile3','otile4'],
+mapquestAttrib = "MapQuest and OpenStreetMap"
+
+var mapquest = L.tileLayer.grayscale(mapquestUrl, {maxZoom: 18, attribution: mapquestAttrib, subdomains: subDomains});
+
+mapquest.addTo(map);
+
+function init () {
+  map.setView(india, 5);
+}
+
+init();
