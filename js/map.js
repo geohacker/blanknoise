1bfeb3bf686e93ec4c1c1dd57a1d81a11cbb0fbf
Sajjad Anwar <sajjadkm@gmail.com>
2014-03-07 23:28:21 +0530

    Circle marker with text and selection change frame

--- js/map.js
+++ js/map.js
@@ -7,11 +7,32 @@ mapquestAttrib = "MapQuest and OpenStreetMap"
 
 var mapquest = L.tileLayer.grayscale(mapquestUrl, {maxZoom: 18, attribution: mapquestAttrib, subdomains: subDomains});
 
-mapquest.addTo(map);
+// mapquest.addTo(map);
+
+// Circle icon
+// var cityIcon = ;
 
 function init () {
   $("#select").select2();
+  $("#select").on("change", countryChanged);
+
+  function countryChanged (selection) {
+    country = selection.val;
+    // Send ajax and get GeoJSON
+
+      }
   map.setView(india, 5);
+
+  // m = L.marker(india, 
+  //   {icon: L.divIcon({className: 'icon', html: 10, iconSize: [40, 40]})}
+  //   ).addTo(map);
+  // m.on({
+  //   click: function (e) {
+  //     console.log(e);
+  //   }
+  // });
+
+
 }
 
 init();
